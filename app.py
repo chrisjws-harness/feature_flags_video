4dea7b8f4f45dba87b60c7745e3feb7457f70991
Chris Storz <chris.storz@harness.io>
2023-09-21 07:02:40 -0700

    function change

--- app.py
+++ app.py
@@ -4,25 +4,31 @@ import os
 
 app = Flask(__name__)
 
+def get_weather(city):
+    api_key = os.environ.get('OPENWEATHERMAP_API_KEY')
+    if not api_key:
+        return None, "OpenWeatherMap API key is missing."
+
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=imperial'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        temperature = data['main']['temp']
+        return temperature, None
+    else:
+        return None, "City not found."
+
 @app.route('/v1/temperature')
 def get_temperature():
     city = request.args.get('city')
     if not city:
         return jsonify({"error": "City parameter is missing."}), 400
 
-    api_key = os.environ.get('OPENWEATHERMAP_API_KEY')
-    if not api_key:
-        return jsonify({"error": "OpenWeatherMap API key is missing."}), 500
+    temperature, error = get_weather(city)
+    if error:
+        return jsonify({"error": error}), 404 if error == "City not found." else 500
 
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        data = response.json()
-        temperature = data['main']['temp']
-        return jsonify({"city": city, "temperature": temperature})
-    else:
-        return jsonify({"error": "City not found."}), 404
+    return jsonify({"city": city, "temperature": temperature})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
